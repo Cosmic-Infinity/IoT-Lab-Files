f111710483632d1f00039d810d9fb4ba16969391
Infinity (KIIT) <64971616+Cosmic-Infinity@users.noreply.github.com>
2024-01-20 21:16:09 +0530

    Create adafruit_io_test.ino

---
+++ adafruit_io_test/adafruit_io_test.ino
@@ -0,0 +1,53 @@
+#include <ESP8266WiFi.h>
+#include "AdafruitIO_WiFi.h"
+ 
+#define WIFI_SSID       "somethingsomething"
+#define WIFI_PASS       "somethingsomething"
+ 
+#define IO_USERNAME  "somethingsomething"
+#define IO_KEY       "somethingsomething"
+ 
+ int i = 0;
+// Connect to Wi-Fi and Adafruit IO handel 
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+ 
+// Create a feed object that allows us to send data to
+AdafruitIO_Feed *sending_data = io.feed("Test");
+ 
+ 
+void setup() 
+{
+  // Enable the serial port so we can see updates
+  Serial.begin(115200);
+ 
+  // Connect to Adafruit IO
+  io.connect();
+ 
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(500);
+  }
+}
+ 
+void loop() 
+{
+  // Always keep this at the top of your main loop
+  // While not confirmed, this implies that the Adafruit IO library is not event-driven
+  // This means you should refrain from using infinite loops
+  io.run();
+ 
+  // Send 10 to our Workshop Temperature Feed
+
+    sending_data->save(i);
+    Serial.print("Data sent");
+    Serial.println(i);
+    i++;
+ 
+  // This is so we can see some response on the computer
+  
+ 
+  // DONT SEND A BILLION DATA POINTS! This slows down the program so that it sends readings once every 5 seconds
+  delay(3000);
+}
